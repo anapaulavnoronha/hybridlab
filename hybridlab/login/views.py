39d246fc423027ff07b72e36d41b8a5d6aeaea34
Yago Regis <yyagoregess37@gmail.com>
2014-10-01 19:38:58 -0300

    Arrumando login

--- hybridlab/login/views.py
+++ hybridlab/login/views.py
@@ -4,4 +4,4 @@ from django.template import RequestContext
 # Create your views here.
 
 def login(request):
-	return render_to_response('login/login.html', context_instance=RequestContext(request))
+	return render_to_response('login.html', context_instance=RequestContext(request))
