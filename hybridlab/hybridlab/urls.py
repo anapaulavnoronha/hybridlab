3242522c31bdf744d1b6cd8f71f4363dafae0a22
Yago Regis <yyagoregess37@gmail.com>
2014-10-01 19:20:04 -0300

    Adicionando app login

--- hybridlab/hybridlab/urls.py
+++ hybridlab/hybridlab/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'core.views.home', name='home'),
+    #url(r'^$', 'login.views.login' name='login'),
     # url(r'^$', 'hybridlab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
