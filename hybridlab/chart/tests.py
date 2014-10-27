7824bcfa10010e3829df5b18e92732511a9bd3a5
Yago Regis <yyagoregess37@gmail.com>
2014-10-27 20:26:27 -0200

    Adicionando testes extras para simulação

--- hybridlab/chart/tests.py
+++ hybridlab/chart/tests.py
@@ -19,14 +19,14 @@ class ListSimulationViewTest(TestCase):
 class ResultadosViewTest(TestCase):
 	def test_response_resultados(self):
 		client = Client()
-		Simulation.objects.create()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
 		s = Simulation.objects.get(id=1)
 		response = client.get(reverse('resultados', args = [s.id]))
 		self.failUnlessEqual(response.status_code, 200)
 
 	def test_template_resultados(self):
 		client = Client()
-		Simulation.objects.create()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
 		s = Simulation.objects.get(id=1)
 		response = client.get(reverse('resultados', args = [s.id]))
 		self.assertTemplateUsed(response.status_code, 'resultados.html', 'base.html')
@@ -34,6 +34,27 @@ class ResultadosViewTest(TestCase):
 class SimulationModelTest(TestCase):
 	def test_simulation_exists(self):
 		client = Client()
-		Simulation.objects.create()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
 		s = Simulation.objects.get(id=1)
 		self.assertEqual(s.id, 1)
+		self.assertEqual(s.angulation, 7)
+		self.assertEqual(s.speed, 80)
+		self.assertEqual(s.power, 35)
+
+	def test_simulation_angulation(self):
+		client = Client()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
+		s = Simulation.objects.get(id=1)
+		self.assertEqual(s.angulation, 7)
+
+	def test_simulation_speed(self):
+		client = Client()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
+		s = Simulation.objects.get(id=1)
+		self.assertEqual(s.speed, 80)
+
+	def test_simulation_power(self):
+		client = Client()
+		Simulation.objects.create(angulation=7, speed=80, power=35)
+		s = Simulation.objects.get(id=1)
+		self.assertEqual(s.power, 35)
